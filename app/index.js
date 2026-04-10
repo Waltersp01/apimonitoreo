6ac9f06f4c3e34d0bc59354ff1e80b90fcb4cd0c
Waltersp01 <walter.santos9401@gmail.com>
2026-04-09 21:02:00 -0500

    cambio puerto api2

--- app/index.js
+++ app/index.js
@@ -3,79 +3,33 @@ const client = require("prom-client");
 
 const app = express();
 
-app.use(express.json());
-
-
 client.collectDefaultMetrics();
-
 
 const counter = new client.Counter({
   name: "http_requests_total",
-  help: "Total de peticiones HTTP",
-  labelNames: ["method", "route"]
+  help: "Total de peticiones",
 });
 
-
-const httpDuration = new client.Histogram({
-  name: "http_request_duration_ms",
-  help: "Duración de peticiones en ms",
-  buckets: [50, 100, 300, 500, 1000, 3000]
-});
-
-
-app.use((req, res, next) => {
-  const start = Date.now();
-
- 
-  counter.labels(req.method, req.path).inc();
-
-
-  res.on("finish", () => {
-    const duration = Date.now() - start;
-    httpDuration.observe(duration);
-  });
-
-  next();
-});
-
-
-
+// SOLO incrementa en rutas específicas (estable)
 app.get("/", (req, res) => {
-  res.send("API REST con Prometheus ");
+  counter.inc();
+  res.send("Hola mundo 🚀");
 });
-
 
 app.get("/usuarios", (req, res) => {
-  res.json([
-    { id: 1, nombre: "Juan" },
-    { id: 2, nombre: "Ana" }
-  ]);
+  counter.inc();
+  res.json([{ id: 1, nombre: "Juan" }]);
 });
-
 
 app.get("/productos", (req, res) => {
-  res.json([
-    { id: 1, nombre: "Laptop" },
-    { id: 2, nombre: "Mouse" }
-  ]);
+  counter.inc();
+  res.json([{ id: 1, nombre: "Laptop" }]);
 });
 
-
-app.post("/usuarios", (req, res) => {
-  const usuario = req.body;
-  res.status(201).json({
-    mensaje: "Usuario creado",
-    data: usuario
-  });
-});
-
-
+// metrics limpio
 app.get("/metrics", async (req, res) => {
   res.set("Content-Type", client.register.contentType);
   res.end(await client.register.metrics());
 });
 
-
-app.listen(3000, () => {
-  console.log("Servidor corriendo en http://localhost:3000");
-});
+app.listen(3000, () => console.log("API en puerto 3000"));
